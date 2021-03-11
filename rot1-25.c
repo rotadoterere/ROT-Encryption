5b8ef1662efa152a17f18a83a5a002025d054edd
Rota do Tereré <78173840+rotadoterere@users.noreply.github.com>
2021-03-10 23:41:29 -0300

    Update rot1-25.c

--- rot1-25.c
+++ rot1-25.c
@@ -20,7 +20,9 @@ int main(int argc, char *argv[]){
   strcpy(str, argv[1]);
 
   for(int k = 0; k < 25; k++){
+      
     printf("%d\t", k + 1);
+      
     for(int i = 0; str[i] != '\0'; i++){
       if(str[i] < 65 || str[i] > 122){
         continue;
@@ -28,14 +30,16 @@ int main(int argc, char *argv[]){
       if(str[i] == 32)
         i++;
       if(str[i] == 122)
-         str[i] = 97;
+        str[i] = 97;
       else
-       if(str[i] == 90)
-         str[i] = 65;
-       else
-        str[i] += 1;
+        if(str[i] == 90)
+            str[i] = 65;
+        else
+            str[i] += 1;
     }
+      
     printf("%s\n", str);
+      
   }
   
   
